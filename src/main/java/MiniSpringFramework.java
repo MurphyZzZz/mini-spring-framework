8564b340feec9fa736c6a5124b87df75aed3b0df
mingman.zheng <mingman.zheng@thoughtworks.com>
2021-08-15 11:53:43 +0800

    init project

---
+++ src/main/java/MiniSpringFramework.java
@@ -0,0 +1,5 @@
+public class MiniSpringFramework {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/fixture/ExampleTest.java
@@ -0,0 +1,36 @@
+package fixture;
+
+import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
+import org.junit.jupiter.api.Test;
+
+@FuShengTest
+public class ExampleTest {
+    int minWager;
+    String players;
+    int pot = 0;
+
+    @Test
+    void name() {
+
+    }
+
+    public void newGame(String players){
+        this.players = players;
+    }
+
+    public void setMinWager(int minWager) {
+        this.minWager = minWager;
+    }
+
+    public void bet() {
+        this.pot += minWager;
+    }
+
+    public void raise(int wager) {
+        this.pot += (minWager + wager);
+    }
+
+    public int getPot(){
+        return this.pot;
+    }
+}
